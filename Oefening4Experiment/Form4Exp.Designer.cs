96f1419d5a7a61b18da56bc2f773b2100013a1b1
Smokair <dcuo1030@outlook.com>
2022-10-02 17:30:15 +0200

    experiment oef4

---
+++ Oefening4Experiment/Form4Exp.Designer.cs
@@ -0,0 +1,411 @@
+﻿namespace Oefening4Experiment
+{
+    partial class Form4Exp
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.calculatebtn = new System.Windows.Forms.Button();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label_sales_tax = new System.Windows.Forms.Label();
+            this.label_shipping = new System.Windows.Forms.Label();
+            this.label_grand_total = new System.Windows.Forms.Label();
+            this.label_tax_rate = new System.Windows.Forms.Label();
+            this.label_subtotal = new System.Windows.Forms.Label();
+            this.id_grand_total = new System.Windows.Forms.TextBox();
+            this.id_sales_tax = new System.Windows.Forms.TextBox();
+            this.id_shipping = new System.Windows.Forms.TextBox();
+            this.id_tax_rate = new System.Windows.Forms.TextBox();
+            this.id_subtotal = new System.Windows.Forms.TextBox();
+            this.ext_hat = new System.Windows.Forms.TextBox();
+            this.ext_skis = new System.Windows.Forms.TextBox();
+            this.ext_boots = new System.Windows.Forms.TextBox();
+            this.ext_gloves = new System.Windows.Forms.TextBox();
+            this.hat_price = new System.Windows.Forms.TextBox();
+            this.skis_price = new System.Windows.Forms.TextBox();
+            this.boots_price = new System.Windows.Forms.TextBox();
+            this.gloves_price = new System.Windows.Forms.TextBox();
+            this.id_hat = new System.Windows.Forms.TextBox();
+            this.id_skis = new System.Windows.Forms.TextBox();
+            this.id_boots = new System.Windows.Forms.TextBox();
+            this.id_gloves = new System.Windows.Forms.TextBox();
+            this.ext_price = new System.Windows.Forms.Label();
+            this.id_price = new System.Windows.Forms.Label();
+            this.id_quantity = new System.Windows.Forms.Label();
+            this.id_item = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // calculatebtn
+            // 
+            this.calculatebtn.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.calculatebtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.calculatebtn.Location = new System.Drawing.Point(218, 302);
+            this.calculatebtn.Name = "calculatebtn";
+            this.calculatebtn.Size = new System.Drawing.Size(121, 31);
+            this.calculatebtn.TabIndex = 64;
+            this.calculatebtn.Text = "Calculate";
+            this.calculatebtn.UseVisualStyleBackColor = false;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(324, 113);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(53, 23);
+            this.numericUpDown2.TabIndex = 63;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(324, 142);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(53, 23);
+            this.numericUpDown3.TabIndex = 62;
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(324, 170);
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(53, 23);
+            this.numericUpDown4.TabIndex = 61;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(324, 84);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(53, 23);
+            this.numericUpDown1.TabIndex = 60;
+            // 
+            // label_sales_tax
+            // 
+            this.label_sales_tax.AutoSize = true;
+            this.label_sales_tax.Location = new System.Drawing.Point(379, 310);
+            this.label_sales_tax.Name = "label_sales_tax";
+            this.label_sales_tax.Size = new System.Drawing.Size(53, 15);
+            this.label_sales_tax.TabIndex = 59;
+            this.label_sales_tax.Text = "Sales Tax";
+            // 
+            // label_shipping
+            // 
+            this.label_shipping.AutoSize = true;
+            this.label_shipping.Location = new System.Drawing.Point(379, 339);
+            this.label_shipping.Name = "label_shipping";
+            this.label_shipping.Size = new System.Drawing.Size(54, 15);
+            this.label_shipping.TabIndex = 58;
+            this.label_shipping.Text = "Shipping";
+            // 
+            // label_grand_total
+            // 
+            this.label_grand_total.AutoSize = true;
+            this.label_grand_total.Location = new System.Drawing.Point(379, 370);
+            this.label_grand_total.Name = "label_grand_total";
+            this.label_grand_total.Size = new System.Drawing.Size(67, 15);
+            this.label_grand_total.TabIndex = 57;
+            this.label_grand_total.Text = "Grand Total";
+            // 
+            // label_tax_rate
+            // 
+            this.label_tax_rate.AutoSize = true;
+            this.label_tax_rate.Location = new System.Drawing.Point(379, 281);
+            this.label_tax_rate.Name = "label_tax_rate";
+            this.label_tax_rate.Size = new System.Drawing.Size(50, 15);
+            this.label_tax_rate.TabIndex = 56;
+            this.label_tax_rate.Text = "Tax Rate";
+            // 
+            // label_subtotal
+            // 
+            this.label_subtotal.AutoSize = true;
+            this.label_subtotal.Location = new System.Drawing.Point(379, 252);
+            this.label_subtotal.Name = "label_subtotal";
+            this.label_subtotal.Size = new System.Drawing.Size(51, 15);
+            this.label_subtotal.TabIndex = 55;
+            this.label_subtotal.Text = "Subtotal";
+            // 
+            // id_grand_total
+            // 
+            this.id_grand_total.Location = new System.Drawing.Point(482, 362);
+            this.id_grand_total.Name = "id_grand_total";
+            this.id_grand_total.Size = new System.Drawing.Size(100, 23);
+            this.id_grand_total.TabIndex = 54;
+            this.id_grand_total.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // id_sales_tax
+            // 
+            this.id_sales_tax.Location = new System.Drawing.Point(482, 302);
+            this.id_sales_tax.Name = "id_sales_tax";
+            this.id_sales_tax.Size = new System.Drawing.Size(100, 23);
+            this.id_sales_tax.TabIndex = 53;
+            this.id_sales_tax.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // id_shipping
+            // 
+            this.id_shipping.Location = new System.Drawing.Point(482, 331);
+            this.id_shipping.Name = "id_shipping";
+            this.id_shipping.Size = new System.Drawing.Size(100, 23);
+            this.id_shipping.TabIndex = 52;
+            this.id_shipping.Text = "€ 5.00";
+            this.id_shipping.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // id_tax_rate
+            // 
+            this.id_tax_rate.Location = new System.Drawing.Point(482, 273);
+            this.id_tax_rate.Name = "id_tax_rate";
+            this.id_tax_rate.Size = new System.Drawing.Size(100, 23);
+            this.id_tax_rate.TabIndex = 51;
+            this.id_tax_rate.Text = "0.10%";
+            this.id_tax_rate.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // id_subtotal
+            // 
+            this.id_subtotal.Location = new System.Drawing.Point(482, 244);
+            this.id_subtotal.Name = "id_subtotal";
+            this.id_subtotal.Size = new System.Drawing.Size(100, 23);
+            this.id_subtotal.TabIndex = 50;
+            this.id_subtotal.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // ext_hat
+            // 
+            this.ext_hat.Location = new System.Drawing.Point(482, 141);
+            this.ext_hat.Name = "ext_hat";
+            this.ext_hat.Size = new System.Drawing.Size(100, 23);
+            this.ext_hat.TabIndex = 49;
+            this.ext_hat.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // ext_skis
+            // 
+            this.ext_skis.Location = new System.Drawing.Point(482, 170);
+            this.ext_skis.Name = "ext_skis";
+            this.ext_skis.Size = new System.Drawing.Size(100, 23);
+            this.ext_skis.TabIndex = 48;
+            this.ext_skis.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // ext_boots
+            // 
+            this.ext_boots.Location = new System.Drawing.Point(482, 112);
+            this.ext_boots.Name = "ext_boots";
+            this.ext_boots.Size = new System.Drawing.Size(100, 23);
+            this.ext_boots.TabIndex = 47;
+            this.ext_boots.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // ext_gloves
+            // 
+            this.ext_gloves.Location = new System.Drawing.Point(482, 83);
+            this.ext_gloves.Name = "ext_gloves";
+            this.ext_gloves.Size = new System.Drawing.Size(100, 23);
+            this.ext_gloves.TabIndex = 46;
+            this.ext_gloves.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // hat_price
+            // 
+            this.hat_price.Location = new System.Drawing.Point(409, 141);
+            this.hat_price.Name = "hat_price";
+            this.hat_price.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.hat_price.Size = new System.Drawing.Size(61, 23);
+            this.hat_price.TabIndex = 45;
+            this.hat_price.Text = "6";
+            // 
+            // skis_price
+            // 
+            this.skis_price.Location = new System.Drawing.Point(409, 170);
+            this.skis_price.Name = "skis_price";
+            this.skis_price.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.skis_price.Size = new System.Drawing.Size(61, 23);
+            this.skis_price.TabIndex = 44;
+            this.skis_price.Text = "700";
+            // 
+            // boots_price
+            // 
+            this.boots_price.Location = new System.Drawing.Point(409, 112);
+            this.boots_price.Name = "boots_price";
+            this.boots_price.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.boots_price.Size = new System.Drawing.Size(61, 23);
+            this.boots_price.TabIndex = 43;
+            this.boots_price.Text = "30";
+            // 
+            // gloves_price
+            // 
+            this.gloves_price.Location = new System.Drawing.Point(409, 83);
+            this.gloves_price.Name = "gloves_price";
+            this.gloves_price.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.gloves_price.Size = new System.Drawing.Size(61, 23);
+            this.gloves_price.TabIndex = 42;
+            this.gloves_price.Text = "10";
+            // 
+            // id_hat
+            // 
+            this.id_hat.Location = new System.Drawing.Point(218, 141);
+            this.id_hat.Name = "id_hat";
+            this.id_hat.Size = new System.Drawing.Size(100, 23);
+            this.id_hat.TabIndex = 41;
+            this.id_hat.Text = "Hat";
+            // 
+            // id_skis
+            // 
+            this.id_skis.Location = new System.Drawing.Point(218, 170);
+            this.id_skis.Name = "id_skis";
+            this.id_skis.Size = new System.Drawing.Size(100, 23);
+            this.id_skis.TabIndex = 40;
+            this.id_skis.Text = "Skis";
+            // 
+            // id_boots
+            // 
+            this.id_boots.Location = new System.Drawing.Point(218, 112);
+            this.id_boots.Name = "id_boots";
+            this.id_boots.Size = new System.Drawing.Size(100, 23);
+            this.id_boots.TabIndex = 39;
+            this.id_boots.Text = "Boots";
+            // 
+            // id_gloves
+            // 
+            this.id_gloves.Location = new System.Drawing.Point(218, 83);
+            this.id_gloves.Name = "id_gloves";
+            this.id_gloves.Size = new System.Drawing.Size(100, 23);
+            this.id_gloves.TabIndex = 38;
+            this.id_gloves.Text = "Gloves";
+            // 
+            // ext_price
+            // 
+            this.ext_price.AutoSize = true;
+            this.ext_price.Location = new System.Drawing.Point(502, 65);
+            this.ext_price.Name = "ext_price";
+            this.ext_price.Size = new System.Drawing.Size(55, 15);
+            this.ext_price.TabIndex = 37;
+            this.ext_price.Text = "Ext. Price";
+            // 
+            // id_price
+            // 
+            this.id_price.AutoSize = true;
+            this.id_price.Location = new System.Drawing.Point(409, 65);
+            this.id_price.Name = "id_price";
+            this.id_price.Size = new System.Drawing.Size(61, 15);
+            this.id_price.TabIndex = 36;
+            this.id_price.Text = "Price Each";
+            // 
+            // id_quantity
+            // 
+            this.id_quantity.AutoSize = true;
+            this.id_quantity.Location = new System.Drawing.Point(324, 65);
+            this.id_quantity.Name = "id_quantity";
+            this.id_quantity.Size = new System.Drawing.Size(53, 15);
+            this.id_quantity.TabIndex = 35;
+            this.id_quantity.Text = "Quantity";
+            // 
+            // id_item
+            // 
+            this.id_item.AutoSize = true;
+            this.id_item.Location = new System.Drawing.Point(250, 65);
+            this.id_item.Name = "id_item";
+            this.id_item.Size = new System.Drawing.Size(31, 15);
+            this.id_item.TabIndex = 34;
+            this.id_item.Text = "Item";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.calculatebtn);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label_sales_tax);
+            this.Controls.Add(this.label_shipping);
+            this.Controls.Add(this.label_grand_total);
+            this.Controls.Add(this.label_tax_rate);
+            this.Controls.Add(this.label_subtotal);
+            this.Controls.Add(this.id_grand_total);
+            this.Controls.Add(this.id_sales_tax);
+            this.Controls.Add(this.id_shipping);
+            this.Controls.Add(this.id_tax_rate);
+            this.Controls.Add(this.id_subtotal);
+            this.Controls.Add(this.ext_hat);
+            this.Controls.Add(this.ext_skis);
+            this.Controls.Add(this.ext_boots);
+            this.Controls.Add(this.ext_gloves);
+            this.Controls.Add(this.hat_price);
+            this.Controls.Add(this.skis_price);
+            this.Controls.Add(this.boots_price);
+            this.Controls.Add(this.gloves_price);
+            this.Controls.Add(this.id_hat);
+            this.Controls.Add(this.id_skis);
+            this.Controls.Add(this.id_boots);
+            this.Controls.Add(this.id_gloves);
+            this.Controls.Add(this.ext_price);
+            this.Controls.Add(this.id_price);
+            this.Controls.Add(this.id_quantity);
+            this.Controls.Add(this.id_item);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button calculatebtn;
+        private NumericUpDown numericUpDown2;
+        private NumericUpDown numericUpDown3;
+        private NumericUpDown numericUpDown4;
+        private NumericUpDown numericUpDown1;
+        private Label label_sales_tax;
+        private Label label_shipping;
+        private Label label_grand_total;
+        private Label label_tax_rate;
+        private Label label_subtotal;
+        private TextBox id_grand_total;
+        private TextBox id_sales_tax;
+        private TextBox id_shipping;
+        private TextBox id_tax_rate;
+        private TextBox id_subtotal;
+        private TextBox ext_hat;
+        private TextBox ext_skis;
+        private TextBox ext_boots;
+        private TextBox ext_gloves;
+        private TextBox hat_price;
+        private TextBox skis_price;
+        private TextBox boots_price;
+        private TextBox gloves_price;
+        private TextBox id_hat;
+        private TextBox id_skis;
+        private TextBox id_boots;
+        private TextBox id_gloves;
+        private Label ext_price;
+        private Label id_price;
+        private Label id_quantity;
+        private Label id_item;
+    }
+}
---
+++ Oefening4Experiment/Form4Exp.cs
@@ -0,0 +1,11 @@
+namespace Oefening4Experiment
+{
+    public partial class Form4Exp : Form
+    {
+        public Form4Exp()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
