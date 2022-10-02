7ac5eb7a147864693d05907e2899b472882057c8
Smokair <dcuo1030@outlook.com>
2022-10-02 17:24:09 +0200

    oef 5 hermaken

--- Oefening5/Form5.Designer.cs
+++ Oefening5/Form5.Designer.cs
@@ -29,76 +29,77 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.ctof = new System.Windows.Forms.Button();
+            this.textC = new System.Windows.Forms.TextBox();
+            this.textF = new System.Windows.Forms.TextBox();
+            this.ftoc = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(166, 116);
+            this.label1.Location = new System.Drawing.Point(25, 74);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(44, 15);
             this.label1.TabIndex = 0;
             this.label1.Text = "Celsius";
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(139, 134);
-            this.textBox1.Name = "textc";
-            this.textBox1.Size = new System.Drawing.Size(100, 23);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(139, 163);
-            this.button1.Name = "ctof";
-            this.button1.Size = new System.Drawing.Size(100, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "C to F";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(245, 163);
-            this.button2.Name = "ftoc";
-            this.button2.Size = new System.Drawing.Size(100, 23);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "F to C";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(245, 134);
-            this.textBox2.Name = "textf";
-            this.textBox2.Size = new System.Drawing.Size(100, 23);
-            this.textBox2.TabIndex = 4;
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(262, 116);
+            this.label2.Location = new System.Drawing.Point(25, 167);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(63, 15);
-            this.label2.TabIndex = 3;
+            this.label2.TabIndex = 1;
             this.label2.Text = "Fahrenheit";
             // 
-            // Form1
+            // ctof
+            // 
+            this.ctof.Location = new System.Drawing.Point(94, 100);
+            this.ctof.Name = "ctof";
+            this.ctof.Size = new System.Drawing.Size(100, 23);
+            this.ctof.TabIndex = 2;
+            this.ctof.Text = "Convert C to F";
+            this.ctof.UseVisualStyleBackColor = true;
+            this.ctof.Click += new System.EventHandler(this.ctof_Click);
+            // 
+            // textC
+            // 
+            this.textC.Location = new System.Drawing.Point(94, 71);
+            this.textC.Name = "textC";
+            this.textC.Size = new System.Drawing.Size(100, 23);
+            this.textC.TabIndex = 3;
+            // 
+            // textF
+            // 
+            this.textF.Location = new System.Drawing.Point(94, 164);
+            this.textF.Name = "textF";
+            this.textF.Size = new System.Drawing.Size(100, 23);
+            this.textF.TabIndex = 4;
+            // 
+            // ftoc
+            // 
+            this.ftoc.Location = new System.Drawing.Point(94, 135);
+            this.ftoc.Name = "ftoc";
+            this.ftoc.Size = new System.Drawing.Size(100, 23);
+            this.ftoc.TabIndex = 5;
+            this.ftoc.Text = "Convert F to C";
+            this.ftoc.UseVisualStyleBackColor = true;
+            this.ftoc.Click += new System.EventHandler(this.ftoc_Click);
+            // 
+            // Form5
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.ftoc);
+            this.Controls.Add(this.textF);
+            this.Controls.Add(this.textC);
+            this.Controls.Add(this.ctof);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
-            this.Name = "Form1";
+            this.Name = "Form5";
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -108,10 +109,10 @@
         #endregion
 
         private Label label1;
-        private TextBox textBox1;
-        private Button button1;
-        private Button button2;
-        private TextBox textBox2;
         private Label label2;
+        private Button ctof;
+        private TextBox textC;
+        private TextBox textF;
+        private Button ftoc;
     }
 }
--- Oefening5/Form5.cs
+++ Oefening5/Form5.cs
@@ -5,18 +5,22 @@ namespace Oefening5
         public Form5()
         {
             InitializeComponent();
-            
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1(object sender, EventArgs e)
+        private void ftoc_Click(object sender, EventArgs e)
         {
             double celsius = 0.0, fahrenheit = 0.0;
-            fahrenheit = Convert.ToDouble(textf.Text);
+            fahrenheit = Convert.ToDouble(textF.Text);
+            celsius = (5.0 / 9) * (fahrenheit - 32);
+            textC.Text = celsius.ToString();
+        }
+
+        private void ctof_Click(object sender, EventArgs e)
+        {
+            double celsius = 0.0, fahrenheit = 0.0;
+            celsius = Convert.ToDouble(textC.Text);
+            fahrenheit = celsius * 9 / 5 + 32;
+            textF.Text = fahrenheit.ToString();
         }
     }
 }
