f4dde20ab766b954309c1d6ecdc7ffca3c5488a4
Smokair <dcuo1030@outlook.com>
2022-10-02 16:51:07 +0200

    oef 5 openen

---
+++ Oefening5/Form5.cs
@@ -0,0 +1,17 @@
+namespace Oefening5
+{
+    public partial class Form5 : Form
+    {
+        public Form5()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            int celsius = 0;
+            
+        }
+    }
+}
